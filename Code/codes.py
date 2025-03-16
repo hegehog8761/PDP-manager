92d61a0090f3b7e1b0e0b7c8fe12e1a1641178f0
hegehog8761 <119626198+hegehog8761@users.noreply.github.com>
2025-03-16 22:30:18 +0000

    Creating UI for manager
    Began to create UI for the manager, currently creating a custom colour picker

---
+++ Code/codes.py
@@ -0,0 +1,2 @@
+class mode():
+    off = "21 00 84 01 a1"
---
+++ Code/main.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+import subprocess, usb.core, warnings
+from PIL import ImageTk, Image
+
+
+### Set up tkinter window
+window = tk.Tk()
+window.title("PDP manager")
+
+###   Set up controller selection menu
+
+controllers = []
+
+def get_controllers():
+    global controllers
+    devices = usb.core.find(find_all=True)
+    for device in devices:
+        try:
+            manufacturer = device.manufacturer
+
+            warnings.warn("Currently checking for incorrect manufacturer, ensure to change manufacturer name upon release.")
+            if manufacturer == "Sunplus IT Co ":
+                print("Found usb")
+                controllers.append(device)
+            
+        except ValueError:
+            print("This script must be ran with sudo. Exiting...")
+            exit()
+
+get_controllers()
+
+### Draw controller selection menu
+
+
+### Draw controller management page
+
+def draw_controller_screen():
+    img = ImageTk.PhotoImage(Image.open("/home/william/Documents/PDP control hub/PDP-manager/Code/resources/colour-wheel.png"))
+    img_label = tk.Label(window, image=img)
+   
+    
+    
+    button = tk.Button(window, text="Hello World!")
+    button.pack(padx=10, pady=5)
+
+    img_label.pack(padx=10, pady=10)
+
+draw_controller_screen()
+
+
+
+tk.mainloop()
